113bf3b66c234e59475b2e673b987611307a5084
Sneha Sankavaram <snehasankavaram@berkeley.edu>
2016-04-13 22:30:41 -0700

    removed _ between date and time

--- db/migrate/20160410205750_add_about_to_orgs.rb
+++ db/migrate/20160410205750_add_about_to_orgs.rb
@@ -2,26 +2,26 @@ class AddAboutToOrgs < ActiveRecord::Migration
   def change
   	add_column :organizations, :link, :string
 
-  	add_column :organizations, :sun_open, :date_time
-  	add_column :organizations, :sun_close, :date_time
+  	add_column :organizations, :sun_open, :datetime
+  	add_column :organizations, :sun_close, :datetime
 
-  	add_column :organizations, :mon_open, :date_time
-  	add_column :organizations, :mon_close, :date_time
+  	add_column :organizations, :mon_open, :datetime
+  	add_column :organizations, :mon_close, :datetime
 
-  	add_column :organizations, :tues_open, :date_time
-  	add_column :organizations, :tues_close, :date_time
+  	add_column :organizations, :tues_open, :datetime
+  	add_column :organizations, :tues_close, :datetime
 
-  	add_column :organizations, :wed_open, :date_time
-  	add_column :organizations, :wed_close, :date_time
+  	add_column :organizations, :wed_open, :datetime
+  	add_column :organizations, :wed_close, :datetime
 
-  	add_column :organizations, :thurs_open, :date_time
-  	add_column :organizations, :thurs_close, :date_time
+  	add_column :organizations, :thurs_open, :datetime
+  	add_column :organizations, :thurs_close, :datetime
 
-  	add_column :organizations, :fri_open, :date_time
-  	add_column :organizations, :fri_close, :date_time
+  	add_column :organizations, :fri_open, :datetime
+  	add_column :organizations, :fri_close, :datetime
 
-  	add_column :organizations, :sat_open, :date_time
-  	add_column :organizations, :sat_close, :date_time
+  	add_column :organizations, :sat_open, :datetime
+  	add_column :organizations, :sat_close, :datetime
 
   	add_column :pledges, :org_id, :integer
   end
